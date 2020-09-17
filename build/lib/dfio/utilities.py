c684759dcf5900e74903e824193420fb7d5afb47
Jim Barlow <jim@beepbeep.technology>
2020-09-17 12:52:46 +0200

    Release 0.0.4

--- build/lib/dfio/utilities.py
+++ build/lib/dfio/utilities.py
@@ -1,5 +1,7 @@
 import json
 import logging
+from datetime import date
+from datetime import timedelta
 
 
 def get_local_json_file_as_dict(path_to_file):
@@ -20,3 +22,32 @@ def get_local_json_file_as_dict(path_to_file):
         file_contents = {}
 
     return file_contents
+
+
+def get_today_date(delimiter: str) -> str:
+    """
+    Gets the current date as a formatted string
+    Args:
+        delimiter (string): delimiter to use in the output string
+    Returns:
+        today_formatted (string): the date today in the format YYYY[]MM[]DD, delimited according to the input 'delimiter' argument
+    """
+    today = date.today()
+    today_formatted = today.strftime(f"%Y{delimiter}%m{delimiter}%d")
+
+    return today_formatted
+
+
+def get_yesterday_date(delimiter: str) -> str:
+    """
+    Gets yesterday's date as a formatted string
+    Args:
+        delimiter (string): delimiter to use in the output string
+    Returns:
+        yesterday_formatted (string): the date yesterday in the format YYYY[]MM[]DD, delimited according to the input 'delimiter' argument
+    """
+    today = date.today()
+    yesterday = today - timedelta(days=1)
+    yesterday_formatted = yesterday.strftime(f"%Y{delimiter}%m{delimiter}%d")
+
+    return yesterday_formatted
